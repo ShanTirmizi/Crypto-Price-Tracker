a2c23f6ab83b460f232f66f671e58b9e13234f3e
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-01-11 17:14:31 +0000

    Fetched the data from the api and pushed it inside the coins array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import react, { useEffect, useState } from "react";
 import './App.css';
 
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=GBP&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const fetchCrypto = async () => {
+    try {
+      const response = await fetch(url)
+      const currencies = await response.json()
+      console.log(currencies);
+      setCoins(currencies);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCrypto()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className='coin-text'>
+          Search a currency
+        </h1>
+        <form>
+          <input 
+            type='text'
+            placeholder='Search'
+            className='coin-input' />
+        </form>
+      </div>
     </div>
   );
 }
